cce424cf5ffac5f2f32962cb92bb440128c64c5c
Robert Thao <robert.lee.thao@gmail.com>
2024-05-17 10:16:21 -0500

    Finished gallery styling and individual art display page

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -8,12 +8,13 @@ const Card = ({ route, name, dated, artist, image, description }) => {
     return (
         <Wrapper to={route} >
             <ArtDisplay>
-                <img src={image} />
+                <Painting src={image} />
             </ArtDisplay>
             <InfoSection>
-                <h1>{name} ({dated})</h1>
-                <h2>{artist}</h2>
+                <HeaderText>{name} ({dated})</HeaderText>
+                <SubText>{artist}</SubText>
                 <Details>{description}</Details>
+                <More>Learn More</More>
             </InfoSection>
         </Wrapper>
     )
@@ -22,34 +23,67 @@ const Card = ({ route, name, dated, artist, image, description }) => {
 export default Card;
 
 const Wrapper = styled(Link)`
-    height: 40vh;
-    width: max-content;
-    display: flex;
+    overflow: hidden;
+    color: #fff;
+    padding: 5vh 5vw;
+    &:hover,
+    &:focus {
+        box-shadow: 0 0 10px #fff;
+    };
+    &:active {
+        box-shadow: 0 0 10px 10px #617eff;
+    }
 `
+
+
 const ArtDisplay = styled.div`
     verticle-align: middle;
+    margin-left: auto;
+    margin-right: auto;
     width: max-content;
 `
 
 const Painting = styled.img`
-
+    height: 30vh;
 `
 
 const InfoSection = styled.div`
-    height: max-content;
     width: 30vw;
-    overflow: hidden;
-    color: #000;
+    color: #fff;
 `
 
 const HeaderText = styled.h1`
-
+    font-size: 26px;
+    text-align: center;
 `
 
 const SubText = styled.h2`
-
+    font-size: 20px;
+    text-align: center;
 `
 
 const Details = styled.p`
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+    font-size: 18px;
+`
 
+const More = styled.p`
+    font-size: 20px;
+    margin-top: 2rem;
+    text-align: center;
+    visibility: hidden;
+
+    ${Wrapper}:hover &,
+    ${Wrapper}:focus & {
+        visibility: visible;
+        text-decoration: underline 1px #fff
+    };
+    ${Wrapper}:active & {
+        font-weight: 1000;
+        font-size: 24px;
+        color: #617eff;
+        text-decoration: underline 2px #617eff;
+    }
 `
